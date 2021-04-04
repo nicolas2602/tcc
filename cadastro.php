e0a1b87b76d8cc47b8c979bfb650913d6c3e5b64
nicolas2602 <69517285+nicolas2602@users.noreply.github.com>
2021-04-04 17:44:51 -0300

    Trabalho - LPI e SORC 3°T.I parte 4
    Nome(s): Emmanuel Zague, Marcus Vinícius e Nicolas Hiroki 3°T.I

--- cadastro.php
+++ cadastro.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="template.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -42,14 +43,19 @@
       <label>Confirmar senha</label>
     </div>
 
-    <input type="submit" name="cad">
+    <button type="subimit" class="btn btn-outline-success" name="cad">
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-    </input>
+      Cadastrar
+    </button>
 
+    
   </form>
+  <br>
+  
+  <a type="submit" href="login.php" class="btn btn-outline-info">Voltar</a>
 </div>
 
 </body>
--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="template.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -23,13 +24,21 @@
       <input type="password" name="senha" required="">
       <label>Senha</label>
     </div>
-    <input type="submit" href="#" name="sub">
+
+    
+    <button type="button" class="btn btn-outline-info" name="sub">
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-    </input>
+      Entrar
+    </button>
+
+   
   </form>
+<br>
+   <a type="submit" href="cadastro.php" class="btn btn-outline-success">Cadastrar</a>
+
 </div>
 
 </body>
