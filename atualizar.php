9eaee7bff7c2905273e748182b32d57c2a26f59c
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2021-04-23 21:24:36 -0300

    Excluir

--- atualizar.php
+++ atualizar.php
@@ -126,10 +126,28 @@
                 </script>
          
 
-                <img class="img-thumbnail mx-auto d-block" src="<?php echo $f['imagem']?>" width="100px" height="100px">
-                <input type="file" name="f1">
-                <input type="hidden" name="img1" value="<?php echo $f['imagem']?>">
-               
+                <img class="img-thumbnail mx-auto d-block" src="<?php echo $f['imagem']?>" width="210px" height="210px" id="img">
+                <br/>
+                <input type="file" name="f1" id="file" style="display:none;">
+                <center>
+                <button id="button" class="btn btn-primary" name="button" value="Upload" onclick="FileUpload();">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+                        <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                        <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+                    </svg>
+                   Selecione uma imagem
+                </button>
+                </center>
+                <input type="hidden" name="img1" value="<?php echo $f['imagem']?>" id="upload">
+                
+                <script>
+                
+                function FileUpload() {
+                   document.getElementById("file").click();
+                   const fileChosen = document.getElementById('file-chosen');
+                };
+                
+                </script>
 
                 <div class="checkbox mb-3"><br>
                     <center>
